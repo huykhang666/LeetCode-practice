172a942b3297f898fc2c9453837b391d585ad8d5
Huy Khang Nguyen <huykhang666@gmail.com>
2026-03-24 17:31:04 +0700

    Create 8. Squares of a Sorted Array.java

---
+++ 8. Squares of a Sorted Array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        int left = 0;
+        int right = n-1;
+        int k = n-1;
+
+        while(left <= right) {
+            if(Math.abs(nums[left]) > Math.abs(nums[right])) {
+                res[k] = nums[left] * nums[left];
+                left++;
+            } else {
+                res[k] = nums[right] * nums[right];
+                right--;
+            }
+            k--;
+        }
+        return res;
+    }
+}
